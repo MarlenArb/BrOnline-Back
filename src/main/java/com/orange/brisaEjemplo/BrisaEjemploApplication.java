07a22279b7ed8ad1286d9904afe5d29411985123
DanielSan1990 <65087265+DanielSan1990@users.noreply.github.com>
2020-08-03 12:27:59 +0100

    Add files via upload

--- src/main/java/com/orange/brisaEjemplo/BrisaEjemploApplication.java
+++ src/main/java/com/orange/brisaEjemplo/BrisaEjemploApplication.java
@@ -1,13 +1,22 @@
 package com.orange.brisaEjemplo;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.orange.brisaEjemplo.controller.ClientController;
 
 @SpringBootApplication
 public class BrisaEjemploApplication {
 
+	private static Logger logger = LogManager.getLogger(BrisaEjemploApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BrisaEjemploApplication.class, args);
+		logger.info("---- ClientController.getClients(): Ini");
+		logger.error("---- ClientController.getClients(): Ini");
+		System.out.println("Mandale----------------------------------------------");
 	}
-
+	
 }
